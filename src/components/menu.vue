f376612f2bea91c1bf8c6c10e381be866d6b2955
882059 <2639232484@qq.com>
2023-12-19 23:39:38 +0800

    new

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,6 +1,14 @@
 <template>
-    
+    <main>
+        <div>
+            <li>Mon</li>
+            
+        </div>
+    </main>
 </template>
 
 <script lang="ts">
+// import Mon from './Page1.vue'
+// import Tue from './Page1.vue'
+
 </script>
