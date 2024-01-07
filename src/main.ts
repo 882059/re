f418970b4a27de4abafed81281afa85be42c8e2b
882059 <2639232484@qq.com>
2024-01-07 18:16:40 +0800

    sidebar

--- src/main.ts
+++ src/main.ts
@@ -16,16 +16,16 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes: routes,
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    } else {
-      return {
-        top: 0,
-        behavior: 'auto'
-      }
-    }
-  },
+  // scrollBehavior(to, from, savedPosition) {
+  //   if (savedPosition) {
+  //     return savedPosition
+  //   } else {
+  //     return {
+  //       top: 0,
+  //       behavior: 'auto'
+  //     }
+  //   }
+  // },
 });
 
 export default router
