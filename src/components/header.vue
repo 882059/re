3fdf21f782a7e5eb4a5c561ca7c06aa95053cdc6
882059 <2639232484@qq.com>
2024-02-09 16:10:40 +0800

    fireworks

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,43 +1,43 @@
 <template>
-    <header>
-      <h1>{{ currentRoute }}</h1>
-    </header>
-  </template>
-  <script setup lang="ts">
-  import { currentRoute } from '@/main'
-  </script>
-  <style lang="scss" scoped>
-  header {
+  <header>
+    <h1>{{ currentRoute }}</h1>
+  </header>
+</template>
+<script setup lang="ts">
+import { currentRoute } from '@/main'
+</script>
+<style lang="scss" scoped>
+header {
+  width: 100%;
+  padding: 3rem 0 2.5rem 0;
+
+  .bg {
+    position: absolute;
+    height: 80vh;
     width: 100%;
-    padding: 3rem 0 2.5rem 0;
-  
-    .bg {
-      position: absolute;
-      height: 80vh;
-      width: 100%;
-      left: 0;
-      top: 0;
-    }
-  
+    left: 0;
+    top: 0;
+  }
+
+  h1 {
+    // position: relative;
+    margin: 50px;
+    font-size: 5rem;
+    text-align: center;
+    max-width: 100%;
+    overflow-wrap: break-word;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  header {
     h1 {
-      // position: relative;
-      margin: 50px;
-      font-size: 5rem;
-      text-align: center;
-      max-width: 100%;
-      overflow-wrap: break-word;
+      font-size: 3rem;
     }
   }
-  
-  @media screen and (max-width: 600px) {
-    header {
-      h1 {
-        font-size: 3rem;
-      }
-    }
-  }
-  
-  header h1 {
-    color: black;
-  }
-  </style>
+}
+
+header h1 {
+  color: black;
+}
+</style>
