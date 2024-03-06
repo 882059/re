612264407c316350041b436f51acf43383993d89
882059 <2639232484@qq.com>
2024-03-06 20:29:04 +0800

    new2

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,9 +4,9 @@ import Collaboration from '../views/collaboration.vue'
 import Description from '../views/description.vue'
 
 export const routes = [
-	{component: { Home } , name: 'Home', path: '/zjut-china/'},
-	{component: { Team } , name: 'Team', path: '/zjut-china/team'},
-	{component: { Collaboration } , name: 'Collaboration', path: '/zjut-china/collaboration'},
-	{component: { Description } , name: 'Description', path: '/zjut-china/description'},
+	{component: Home , name: 'Home', path: '/zjut-china/'},
+	{component: Team , name: 'Team', path: '/zjut-china/team'},
+	{component: Collaboration , name: 'Collaboration', path: '/zjut-china/collaboration'},
+	{component: Description , name: 'Description', path: '/zjut-china/description'},
 ]
 export const headings = {'Home': '', 'Team': {'Team': '', 'Collaboration': ''}, 'Project': {'Description': '',}}
